639aa63d546ee7563eba3b705e835b3090fe107e
= <=>
2022-05-05 09:03:42 -0400

    a

--- C1_W4_Assignment.js
+++ C1_W4_Assignment.js
@@ -1,9 +1,12 @@
 let mobilenet;
 let model;
+
 const webcam = new Webcam(document.getElementById('wc'));
 const dataset = new RPSDataset();
 var rockSamples=0, paperSamples=0, scissorsSamples=0, spockSamples=0, lizardSamples=0;
 let isPredicting = false;
+
+//const mobilenet = tf.loadLayersModel('model.json');
 
 async function loadMobilenet() {
   const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_1.0_224/model.json');
@@ -11,83 +14,11 @@ async function loadMobilenet() {
   return tf.model({inputs: mobilenet.inputs, outputs: layer.output});
 }
 
-async function train() {
-  dataset.ys = null;
-  dataset.encodeLabels(5);
-    
-  // In the space below create a neural network that can classify hand gestures
-  // corresponding to rock, paper, scissors, lizard, and spock. The first layer
-  // of your network should be a flatten layer that takes as input the output
-  // from the pre-trained MobileNet model. Since we have 5 classes, your output
-  // layer should have 5 units and a softmax activation function. You are free
-  // to use as many hidden layers and neurons as you like.  
-  // HINT: Take a look at the Rock-Paper-Scissors example. We also suggest
-  // using ReLu activation functions where applicable.
-  model = tf.sequential({
-    layers: [
-        
-      // YOUR CODE HERE
-      tf.layers.flatten({inputShape: mobilenet.outputs[0].shape.slice(1)}),
-      tf.layers.dense({ units: 100, activation: 'relu'}),
-      tf.layers.dense({ units: 5, activation: 'softmax'})
-
-    ]
-  });
-    
-   
-  // Set the optimizer to be tf.train.adam() with a learning rate of 0.0001.
-  const optimizer = tf.train.adam(0.0001);// YOUR CODE HERE
-    
-        
-  // Compile the model using the categoricalCrossentropy loss, and
-  // the optimizer you defined above.
-  model.compile({optimizer: optimizer, loss: 'categoricalCrossentropy'});
- 
-  let loss = 0;
-  model.fit(dataset.xs, dataset.ys, {
-    epochs: 10,
-    callbacks: {
-      onBatchEnd: async (batch, logs) => {
-        loss = logs.loss.toFixed(5);
-        console.log('LOSS: ' + loss);
-        }
-      }
-   });
+async function loadmodel() {
+  const model = await tf.loadLayersModel('my_model.json');
+  return model;
 }
 
-
-function handleButton(elem){
-	switch(elem.id){
-		case "0":
-			rockSamples++;
-			document.getElementById("rocksamples").innerText = "Rock samples:" + rockSamples;
-			break;
-		case "1":
-			paperSamples++;
-			document.getElementById("papersamples").innerText = "Paper samples:" + paperSamples;
-			break;
-		case "2":
-			scissorsSamples++;
-			document.getElementById("scissorssamples").innerText = "Scissors samples:" + scissorsSamples;
-			break;  
-		case "3":
-			spockSamples++;
-			document.getElementById("spocksamples").innerText = "Spock samples:" + spockSamples;
-			break;
-        case "4":
-			lizardSamples++;
-			document.getElementById("lizardsamples").innerText = "Spock samples:" + lizardSamples;
-			break;
-            
-
-		
-            
-	}
-	label = parseInt(elem.id);
-	const img = webcam.capture();
-	dataset.addExample(mobilenet.predict(img), label);
-
-}
 
 async function predict() {
   while (isPredicting) {
@@ -101,19 +32,19 @@ async function predict() {
     var predictionText = "";
     switch(classId){
 		case 0:
-			predictionText = "I see Rock";
+			predictionText = "One";
 			break;
 		case 1:
-			predictionText = "I see Paper";
+			predictionText = "Two";
 			break;
 		case 2:
-			predictionText = "I see Scissors";
+			predictionText = "Three";
 			break;
 		case 3:
-			predictionText = "I see Spock";
+			predictionText = "Four";
 			break;
         case 4:
-			predictionText = "I see Lizard";
+			predictionText = "Five";
 			break;
             
 
@@ -129,10 +60,7 @@ async function predict() {
 }
 
 
-function doTraining(){
-	train();
-	alert("Training Done!")
-}
+
 
 function startPredicting(){
 	isPredicting = true;
@@ -153,6 +81,8 @@ function saveModel(){
 async function init(){
 	await webcam.setup();
 	mobilenet = await loadMobilenet();
+    model = await loadmodel();
+    
 	tf.tidy(() => mobilenet.predict(webcam.capture()));
 		
 }
